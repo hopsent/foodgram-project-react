75010a350f0b60e7e4cc55879ef1f3873ba3baa3
Aleksei Kulakov <law.kulakov@gmail.com>
2022-12-01 01:25:24 +0300

    yet another scrapy mistakes

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,8 +3,7 @@
 from rest_framework import filters
 from rest_framework.exceptions import ValidationError
 
-from recipes.models import Tag
-
+from users.models import User
 
 
 class SpecificAuthorFilterBackend(filters.BaseFilterBackend):
@@ -15,8 +14,10 @@ class SpecificAuthorFilterBackend(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         author = request.query_params.get('author')
-        if not queryset.filter(author=int(author)).exists():
-            raise Exception(
+        if author is None:
+            return queryset
+        elif not User.objects.filter(id=int(author)).exists():
+            raise ValidationError(
                 'Введите корректный id автора.'
             )
         return queryset.filter(author=int(author))
@@ -25,44 +26,37 @@ class SpecificAuthorFilterBackend(filters.BaseFilterBackend):
 class IsFavouritedFilterBackend(filters.BaseFilterBackend):
     """
     To filter queryset with 'is_favorited' field related to
-    :model:'recipes.Favorite'.
+    :model:'recipes.Favorite'. Expected values: 0 or 1.
     """
 
     def filter_queryset(self, request, queryset, view):
         is_favorited = request.query_params.get('is_favorited')
-        if request.user.is_anonymous or is_favorited is None:
+        if (
+            is_favorited is None
+            or request.user.is_anonymous
+            or int(is_favorited) == 0
+        ):
             return queryset
-        elif (not isinstance(is_favorited, int) or int(is_favorited) != 1):
-            raise ValidationError(
-                'Чтобы вывести список избранных рецептов'
-                ' параметр "is_favorited" должен равняться 1.'
-            )
-        elif int(is_favorited) == 1:
-            return queryset.filter(favourite__user=request.user)
+        return queryset.filter(favourite__user=request.user)
 
 
 class IsInShoppingCartFilterBackend(filters.BaseFilterBackend):
     """
     To filter queryset with 'is_in_shopping_cart' field related to
-    :model:'recipes.ShoppingCart'.
+    :model:'recipes.ShoppingCart'. Expected values: 0 or 1.
     """
 
     def filter_queryset(self, request, queryset, view):
         is_in_shopping_cart = request.query_params.get(
             'is_in_shopping_cart'
         )
-        if is_in_shopping_cart is None or request.user.is_anonymous:
-            return queryset
-        elif (
-            not isinstance(is_in_shopping_cart, int)
-            or int(is_in_shopping_cart) != 1
+        if (
+            is_in_shopping_cart is None
+            or request.user.is_anonymous
+            or int(is_in_shopping_cart) == 0
         ):
-            raise ValidationError(
-                'Чтобы вывести список покупок'
-                ' параметр "is_in_shopping_cart" должен равняться 1.'
-            )
-        elif int(is_in_shopping_cart) == 1:
-            return queryset.filter(recipes__user=request.user)
+            return queryset
+        return queryset.filter(recipes__user=request.user)
 
 
 class TagsFilterBackend(filters.BaseFilterBackend):
@@ -73,9 +67,6 @@ class TagsFilterBackend(filters.BaseFilterBackend):
 
     def filter_queryset(self, request, queryset, view):
         tags = request.query_params.get('tags')
-
-        if not Tag.objects.filter(slug=tags).exists():
-            raise Exception(
-                'Введите корректный slug тега.'
-            )
+        if tags is None:
+            return queryset
         return queryset.filter(tags__slug=tags)
