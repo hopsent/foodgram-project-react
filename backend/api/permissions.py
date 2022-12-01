8b55aa6e8bebf708ca23fd323309423c92b1fd40
Aleksei Kulakov <law.kulakov@gmail.com>
2022-12-02 02:31:32 +0300

    permission_recipe changed

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -23,12 +23,16 @@ class IsAuthorOrReadOnly(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or obj.author == request.user)
+        return (
+            request.method in SAFE_METHODS
+            or obj.author == request.user
+        )
 
 
 class IsAuthenticatedOrOwner(BasePermission):
